30740fd0909ba6b59b27d7bb4663afb2259d965b
isabelaJs <isabelajimenezs96@gmail.com>
2021-10-22 14:23:52 -0500

    fix bug 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,13 @@ import { Link } from "gatsby";
 import "../styles/components/notFound.scss";
 
 export default function NotFound() {
-  <div className="mainContainer fillHeight">
-    <h1 className="title">404</h1>
-    <h2 className="subtitle">Page Not Found</h2>
-    <Link className="button button--big" to="/">
-      Go Home
-    </Link>
-  </div>;
+  return (
+    <div className="mainContainer fillHeight">
+      <h1 className="title">404</h1>
+      <h2 className="subtitle">Page Not Found</h2>
+      <Link className="button button--big" to="/">
+        Go Home
+      </Link>
+    </div>
+  );
 }
