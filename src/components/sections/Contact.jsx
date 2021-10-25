0100eb72f7d908d71bb9b27f59af2e6ea8b95a0a
isabelaJs <isabelajimenezs96@gmail.com>
2021-10-24 20:26:21 -0500

    fix bug img about

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -6,13 +6,13 @@ export default function Contact() {
   const revealContainer = useRef(null);
   return (
     <section id='contact' ref={revealContainer}>
-      <h2 className="numbered-heading overline">What’s Next?</h2>
+      <h2 className="numbered-heading overline">¿Qué es lo siguiente?</h2>
 
-      <h2 className="title">Get In Touch</h2>
+      <h2 className="title">Contactame</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        Actualmente estoy buscando nuevas oportunidades y retos, si quieres contactarme mi bandeja de entrada siempre esta disponible.
+        ¡haré lo posible por responderte pronto!
       </p>
 
       <a className="button button--big" href={`mailto:${email}`}>
